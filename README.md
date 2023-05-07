fd22313c07fde9fd5e768b73ab484b6984113c3e
chwang9 <505900598@qq.com>
2023-05-07 11:03:40 +0800

    1

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Vue3-DEMO
-个人练习使用
+# demo
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/login/index.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="login-container">
+    <el-form :model="form" class="login-form">
+        <div class="title-container">
+            <h1 class="title">用户登录</h1>
+        </div>
+        <el-form-item>
+            <svg-icon icon="user" class="svg-container"></svg-icon>
+            <el-input v-model="form.name" />
+        </el-form-item>
+        <el-form-item>
+            <svg-icon icon="password" class="svg-container"></svg-icon>
+            <el-input v-model="form.password" />
+        </el-form-item>
+        <el-button type="primary" @click="onSubmit" class="login-button">用户登录</el-button>
+    </el-form>
+  </div>
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+// do not use same name with ref
+const form = reactive({
+  name: '',
+  password: ''
+})
+
+const onSubmit = () => {
+  console.log('submit!')
+}
+</script>
+
+<style lang="scss" scoped>
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
+$cursor: #fff;
+
+.login-container {
+  min-height: 100%;
+  width: 100%;
+  background-color: $bg;
+  overflow: hidden;
+
+  .login-form {
+    position: relative;
+    width: 520px;
+    max-width: 100%;
+    padding: 160px 35px 0;
+    margin: 0 auto;
+    overflow: hidden;
+
+    .el-form-item {
+      border: 1px solid rgba(255, 255, 255, 0.1);
+      background: rgba(0, 0, 0, 0.1);
+      border-radius: 5px;
+      color: #454545;
+    }
+
+    ::v-deep .el-input {
+      display: inline-block;
+      height: 47px;
+      width: 85%;
+      .el-input__wrapper {
+        background-color: transparent;
+        box-shadow: 0 0 0 0px var(--el-input-border-color,var(--el-border-color)) inset;
+      }
+      input {
+        background: transparent;
+        border: 0px;
+        -webkit-appearance: none;
+        border-radius: 0px;
+        padding: 12px 5px 12px 15px;
+        color: $light_gray;
+        height: 47px;
+        caret-color: $cursor;
+      }
+    }
+    .login-button {
+      width: 100%;
+      box-sizing: border-box;
+    }
+  }
+
+  .tips {
+    font-size: 16px;
+    line-height: 28px;
+    color: #fff;
+    margin-bottom: 10px;
+
+    span {
+      &:first-of-type {
+        margin-right: 16px;
+      }
+    }
+  }
+
+  .svg-container {
+    padding: 6px 5px 6px 15px;
+    color: $dark_gray;
+    vertical-align: middle;
+    display: inline-block;
+  }
+
+  .title-container {
+    position: relative;
+
+    .title {
+      font-size: 26px;
+      color: $light_gray;
+      margin: 0px auto 40px auto;
+      text-align: center;
+      font-weight: bold;
+    }
+
+    ::v-deep .lang-select {
+      position: absolute;
+      top: 4px;
+      right: 0;
+      background-color: white;
+      font-size: 22px;
+      padding: 4px;
+      border-radius: 4px;
+      cursor: pointer;
+    }
+  }
+
+  .show-pwd {
+    // position: absolute;
+    // right: 10px;
+    // top: 7px;
+    font-size: 16px;
+    color: $dark_gray;
+    cursor: pointer;
+    user-select: none;
+  }
+}
+</style>
